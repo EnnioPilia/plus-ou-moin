cb9f4719175a10ed606717d90b76c73d1019759b
EnnioPilia <pilia.ennio@gmail.com>
2025-04-01 14:05:29 +0200

    quatrieme commit

--- Plus ou Moins.js
+++ Plus ou Moins.js
@@ -1,13 +1,19 @@
 let button = document.querySelector("button");
+let randomNumber 
+let answer 
 
-let randomNumber = Math.floor(Math.random() * 100);
+const getRandomNumber = () =>{
+   randomNumber = Math.floor(Math.random() * 100);
 console.log(randomNumber);
-
+}
+   
 const plusMoins = () => {
+   getRandomNumber()
+   
   let numberOfTries = 1;
   console.log("Tries :", numberOfTries);
   
-  let answer = parseInt(prompt("Plus ou moins, devinez le nombre !"));
+  answer = parseInt(prompt("Plus ou moins, devinez le nombre !"));
 
   while (answer !== randomNumber) {
    console.log("Tries :", numberOfTries++);
@@ -15,31 +21,24 @@ const plusMoins = () => {
     if (answer > randomNumber) {
       alert("C'est moins !");
       answer = parseInt(prompt("Essaie encore ! Devine le nombre :"));
-      console.log(answer);
     } else if(answer < randomNumber) {
       alert("C'est plus !");
       answer = parseInt(prompt("Essaie encore ! Devine le nombre :"));
-      console.log(answer);
     } else {
       alert("Ce n'est pas un nombre valide !");
       answer = parseInt(prompt("Essaie encore ! Devine le nombre :"));
-      console.log(answer);
     }
     
     if (answer === randomNumber) {
       alert(`Bravo ! Tu as trouvé en ${numberOfTries} tentatives`);
       let replay = confirm("Rejouer ?" )
-      console.log(replay);
       if (replay === true){
          plusMoins()
+      } else {
+         alert("Merci d'avoir joué !")
       }
-      console.log(answer);
-      
     } 
-  
   }
 };
-
-
 
 button.addEventListener("click", plusMoins);
