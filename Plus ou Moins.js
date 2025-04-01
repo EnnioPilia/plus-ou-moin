d1888052c128f424612584b21273b7226d14bdd2
EnnioPilia <pilia.ennio@gmail.com>
2025-04-01 11:57:48 +0200

    deuxieme commit

---
+++ Plus ou Moins.js
@@ -0,0 +1,38 @@
+let button = document.querySelector("button");
+
+let randomNumber = Math.floor(Math.random() * 100);
+console.log(randomNumber);
+
+const plusMoins = () => {
+  let numberOfTries = 1;
+  console.log("Tries :", numberOfTries);
+  
+  let answer = parseInt(prompt("Plus ou moins, devinez le nombre !"));
+
+  while (answer !== randomNumber) {
+   console.log("Tries :", numberOfTries++);
+
+    if (answer > randomNumber) {
+      alert("C'est moins !");
+      answer = parseInt(prompt("Essaie encore ! Devine le nombre :"));
+      console.log(answer);
+    } else if(answer < randomNumber) {
+      alert("C'est plus !");
+      answer = parseInt(prompt("Essaie encore ! Devine le nombre :"));
+      console.log(answer);
+
+    } else {
+      alert("Caractère invalide !");
+      answer = parseInt(prompt("Essaie encore ! Devine le nombre :"));
+      console.log(answer);
+    }
+    
+    if (answer === randomNumber) {
+     return  alert(`Bravo ! Tu as trouvé en ${numberOfTries} tentatives`);
+    } 
+  }
+};
+
+
+
+button.addEventListener("click", plusMoins);
